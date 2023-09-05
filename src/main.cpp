412cf023ed8b055c7c3973ed8d24464d1652e131
Ewan <EwanBurnettSK@outlook.com>
2023-09-05 17:03:27 +0100

    Random Number Generation
    Implemented a small RNG, capable of generating random floating point
    numbers in a range.

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,14 @@
 #include <stb_image.h>
 #include <thread>
 #include "../include/Progressbar.h"
+#include "../include/RNG.h"
 
 int main()
 {
     printf("Firefly v%d.%da\n", FIREFLY_VERSION_MAJOR, FIREFLY_VERSION_MINOR);
+    Firefly::RNG rng;
+    float rand = rng.RandomFloat(0.0f, 100.0f);
+    printf("\nRandom number: %f\n", rand);
     Firefly::ProgressBar pb(100, "Testing Progress Bar", 40);
 
     for(int i = 0; i < 100; i++){
