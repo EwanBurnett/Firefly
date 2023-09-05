db0fce7034b030858f652e067524fcf07efa497f
Ewan <EwanBurnettSK@outlook.com>
2023-09-05 16:51:25 +0100

    Progress Bar
    Implemented a simple Progress Bar

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,15 @@
 #include <cstdio>
+#include <stb_image.h>
+#include <thread>
+#include "../include/Progressbar.h"
 
 int main()
 {
     printf("Firefly v%d.%da\n", FIREFLY_VERSION_MAJOR, FIREFLY_VERSION_MINOR);
+    Firefly::ProgressBar pb(100, "Testing Progress Bar", 40);
+
+    for(int i = 0; i < 100; i++){
+        pb.Advance();     
+        std::this_thread::sleep_for(std::chrono::milliseconds(50));
+    }
 }
